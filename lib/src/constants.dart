d39e9e3b954476fda34aea22349b3f8f1ad9d89e
samuelezedi <sammy.ezedi@gmail.com>
2021-05-12 16:18:02 +0100

    fixed base url error

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,7 +1,7 @@
 
 import 'package:spotify_web_api/src/scopes.dart';
 
-String spotifyBaseUrl = "https://acounts.spotify.com/api";
+String spotifyBaseUrl = "https://accounts.spotify.com/api";
 
 Map<SpotifyScopes, String> spotifyScopeList = {
   SpotifyScopes.userReadEmail: "user-read-email",
