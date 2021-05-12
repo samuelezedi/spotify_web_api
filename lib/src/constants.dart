1b802a9afe0903e3d0536fd9fb37f5dc15a22142
samuelezedi <sammy.ezedi@gmail.com>
2021-05-12 22:56:00 +0100

    fixed base url slash error

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -3,7 +3,7 @@ import 'package:spotify_web_api/src/scopes.dart';
 
 String spotifyApiBaseUrl = "https://accounts.spotify.com/api";
 
-String spotifyBaseUrl = "https://accounts.spotify.com/";
+String spotifyBaseUrl = "https://accounts.spotify.com";
 
 Map<SpotifyScopes, String> spotifyScopeList = {
   SpotifyScopes.userReadEmail: "user-read-email",
