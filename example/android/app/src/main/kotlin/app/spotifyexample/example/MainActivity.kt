c6a966071db8b833772eb3237c3708f33a14ab28
samuelezedi <sammy.ezedi@gmail.com>
2021-05-13 12:25:22 +0100

    ADDED EXAMPLE COMPLETED

---
+++ example/android/app/src/main/kotlin/app/spotifyexample/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.spotifyexample.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_web_api/spotify_web_api.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: CheckSpot(),
+    );
+  }
+}
+
+class CheckSpot extends StatefulWidget {
+  @override
+  _CheckSpotState createState() => _CheckSpotState();
+}
+
+class _CheckSpotState extends State<CheckSpot> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: InkWell(
+              onTap: ()async{
+                Spotify sp = Spotify(clientID: "a8f9ca92f438485d80b316a8ac067665", clientSecret: "62c6c514cd654ec5962fbc1f9c76816e", redirectUrl: "http://sheepmachine.com/spotify-callback.html");
+                var data = await sp.getAuthorizationCode([SpotifyScopes.playlistReadPrivate], context);
+                print(data);
+                var list = await sp.getAccessToken(data);
+                print(list);
+              },
+              child: Text('Check',style:TextStyle(fontSize: 20, color: Colors.black)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
