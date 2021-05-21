8aad57fb8ebfc6da87e6df1b0e158125ef49e737
Deuque <dchiaha@gmail.com>
2021-05-21 10:06:14 +0100

    added uri to track model

--- lib/src/model/track.dart
+++ lib/src/model/track.dart
@@ -1,8 +1,8 @@
 class Track{
-  String id,name,albumName,previewUrl,spotifyUrl;
+  String id,name,albumName,previewUrl,spotifyUrl,uri;
   List<String> artists;
 
-  Track({this.id,this.name, this.albumName, this.previewUrl, this.spotifyUrl, this.artists});
+  Track({this.id,this.name, this.albumName, this.previewUrl, this.spotifyUrl, this.uri, this.artists});
 
   factory Track.fromMap(Map<String,dynamic> data){
     return Track(
@@ -11,6 +11,7 @@ class Track{
       previewUrl: data['track']['preview_url'],
       albumName: data['track']['album']['name'],
       spotifyUrl: data['track']['external_urls']['spotify'],
+      uri: data['track']['uri'],
       artists: List<String>.from((data['track']['artists'] as List).map((e) => e['name']).toList())
     );
   }
